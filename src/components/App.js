4cdf189fbb06cedfe630d582ed647c6a73316ad2
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2026-06-21 19:56:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-
+import React, { useState } from "react";
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
   return (
+    const [name, setName] = useState("");
+
+  return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>Personalized Greeting</h2>
+
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      {name.trim() !== "" && <h3>Hello, {name}!</h3>}
     </div>
-  )
+  );
 }
+  
+
 
 export default App
